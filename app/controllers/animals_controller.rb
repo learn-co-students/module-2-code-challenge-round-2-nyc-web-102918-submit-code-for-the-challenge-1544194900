9b459e6e4975b946fa0992f116f7cad02e25f0e3
github username <github email address>
2018-12-07 11:20:56 -0500

    done

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,39 @@
 class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal.create(animal_params)
+    if @animal.valid?
+      redirect_to animal_path
+    else
+      flash[:errors] = @animal.errors.full_messages
+      redirect_to new_animal_path
+  end
+end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    @animal.update(animal_params)
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:species_name, :population_size, :average_weight, :habitat_id)
+  end
+
 end
